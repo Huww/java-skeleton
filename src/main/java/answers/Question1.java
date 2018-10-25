ffff53a8c565de6599184458b4f5309f246fa883
Huww <huw.williams051@gmail.com>
2018-10-25 21:48:53 +0100

    Update Question1.java

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,8 +3,15 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+		int ans = 0, len = portfolios.length, x;
+		for (int i=0;i<len;i++) {
+			int p = portfolios[i];
+			for(int j=i+1;j<len;j++){
+				x = p ^ portfolios[j];
+				if (x > ans) ans = x;
+			}
+		}
+		return ans;
 	}
 
 }
