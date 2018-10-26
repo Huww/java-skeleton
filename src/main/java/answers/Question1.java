2637d626e67fb92993a930c0e960a87f4e64658a
Huww <huw.williams051@gmail.com>
2018-10-26 07:51:19 +0100

    Update

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -4,9 +4,9 @@ public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
 		int ans = 0;
-		for (int p1 : portfolios) {
-			for(int p2 : portfolios){
-				int temp = p1 ^ p2;
+		for (int i = 0; i < portfolios.length-1; i++) {
+			for(int j = i + 1; j < portfolios.length; j++){
+				int temp = portfolios[i] ^ portfolios[j];
 				if (temp > ans) {
 					ans = temp;
 				}
